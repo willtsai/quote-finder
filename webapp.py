c44bd6f772278bfa2187efa85cb1d19ad6fc36af
Manuel Suarez Lunar <mansualu@umich.edu>
2023-11-20 22:20:46 -0500

    updating webapp.py

--- webapp.py
+++ webapp.py
@@ -8,31 +8,20 @@ def index():
     return render_template('index.html')
 
 @app.route('/save', methods=['POST']) # Defining the route for the save page
-def save_input():
-    user_input = request.form['user_input']
-    
-    # Save user input to a file
-    with open('user_input.txt', 'w') as file:
-        file.write(user_input)
-
-    # Create an instance of the searcher class and process the input
-    search_instance = searcher('config.toml')
-    search_instance.build_index() # Builds the index
+def save_input(): 
+    user_input = str(request.form['user_input']) # Getting the user input from the form
+    cfg = 'config.toml' # The path to the config file
+    search_instance = searcher(cfg) # Create an instance of the searcher class and process the input
+    idx = search_instance.build_index() # Builds the index
     ranker = search_instance.load_ranker() # Loads the ranker
-    query = search_instance.load_query()  # Loads the query
-    search_instance.run_query(query, search_instance.build_index(), ranker)  # Outputs to output.file
-
-    # Redirecting to the output page
+    user_input = search_instance.sentiment(user_input) # Processes the input
+    query = search_instance.load_query(user_input)  # Loads the query
+    results = search_instance.run_query(query, idx, ranker)  # Runs the query
+    results_string = search_instance.get_results_string(results) # Gets the results as a string
+    session['results_string'] = results_string # Stores the results string in the session
     return redirect(url_for('show_output'))
 
 @app.route('/output', methods=['GET']) # Defining the route for the output page
-def show_output(): 
-  with open('output.txt', 'r') as file:
-            output_text = file.read()
-
-    return render_template('output.html', output_text=output_text)
-
-if __name__ == '__main__':
-    app.run(debug=False)
-
-    
+def show_output(): # Displays the output
+    results_string = session.get('results_string', 'No results found.') # Gets the results string from the session
+    return render_template('output.html', output_text=results_string)
