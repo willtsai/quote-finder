25e2aef779f7f79a648b865f5d76573e7f912719
Will <28876888+willtsai@users.noreply.github.com>
2023-11-29 07:02:52 +0000

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
 # bash script to set up the environment for the project
 #!/bin/bash
 
-conda create -n py35 python=3.5
+# setup conda python 3.5 env for metapy
+# conda update -n base -c defaults conda
+conda create -n py35
 conda activate py35
-# detect if using arm64 or x86_64
+conda install python=3.5
+
+# detect if using arm64
 if [[ $(uname -m) == 'arm64' ]]; then
     conda config --env --set subdir osx-64
+else
+    echo "Using x86_64 architecture"
 fi
+
+# install dependencies
 pip install -r requirements.txt
-. .venv/bin/activate
+
+# crawl and preprocess data
 python web_crawler.py
 python preprocessor.py
 
-echo "Setup complete. Please run \`python web_app.py\` to start the web app."
+# activate flask env for webapp
+. .venv/bin/activate
+
+echo "Setup complete. Please run \`python webapp.py\` to start the web app."
