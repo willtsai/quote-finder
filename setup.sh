d1ccfb9c11a22864ebcbeec318a5e5662adc70b3
willtsai <28876888+willtsai@users.noreply.github.com>
2023-11-28 22:40:42 -0800

    add setup script
    Signed-off-by: willtsai <28876888+willtsai@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 
 conda create -n py35 python=3.5
 conda activate py35
+# detect if using arm64 or x86_64
+if [[ $(uname -m) == 'arm64' ]]; then
+    conda config --env --set subdir osx-64
+fi
 pip install -r requirements.txt
 . .venv/bin/activate
 python web_crawler.py
